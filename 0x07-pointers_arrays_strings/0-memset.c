8d6bbeaf111d7c58a36e63ebc310be1d19b7cfd5
JosseWasonga <jossewasonga@gmail.com>
2022-07-18 09:51:06 -0700

    Function that fills memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,19 +2,18 @@
 #include <string.h>
 
 /**
- * _memset -> this memory set function
- * @s: string
- * @b: a charcter
- * @n: an integer
- * Return: a string
- */
-char *_memcpy(char *dest, char *src, unsigned int n)
+* _memset -> this memory set function
+* @s: string
+* @b: a charcter
+* @n: an integer
+* Return:  a string
+*/
+char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int a;
+	unsigned int a;
 
-for (a = 0; a < n; a++)
-{
-	s[a] = b;
-}
-return (s);
+	for (a = 0; a < n; a++)
+		s[a] = b;
+
+	return (s);
 }
