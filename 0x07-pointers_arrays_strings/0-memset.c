80e83b707a329176e8f08bc0aa492242924aa897
JosseWasonga <jossewasonga@gmail.com>
2022-07-19 12:06:17 -0700

    Function that fills memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,13 +11,14 @@
  * Return: A pointer to the memory area s.
  */
 
-char *_memset(char *s, char b, unsigned int i)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
 
-	for (i = 0; i < n; i++)
+	while (i < n)
 	{
 		s[i] = b;
+		i++;
 	}
 	return (s);
 }
