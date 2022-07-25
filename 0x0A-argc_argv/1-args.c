dfc3fe1ea23dff04daa3a83c2c2afeae560a8c65
JosseWasonga <jossewasonga@gmail.com>
2022-07-25 00:47:13 -0700

    print a number followed by a new line

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
- * main -  writes its name
- * @argc: arguement count
- * @argv: array of pointers to arguement strings
+ * main - prints the number of arguments passed to it
+ * @argc: argument count
+ * @argv: array of pointer to argument strings
  * Return: 0
  **/
 int main(int argc, char *argv[])
 {
-	argc = argc;
-	printf("%s\n", *argv);
+/* to suppress unused variable warning */
+	argv = argv;
+	printf("%d\n", argc - 1);
 	return (0);
 }
